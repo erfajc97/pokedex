37232b9303fe51a81f054ba9aa40fd1fd677288c
Erick <erfacrypto@gmail.com>
2023-01-20 22:24:36 -0300

    [add] - barrita

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App";
 import store from "./store";
 // 
 import { Provider } from "react-redux";
-import "@madzadev/audio-player/dist/index.css";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
